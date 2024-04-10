9e5bcb8ecf7946354f77aba0352f5a5a2d5555d5
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2024-04-10 07:19:40 +0530

    nice

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ from Firebase.firebase import *
 import json
 from datetime import date
 import finnhub
-from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from numpy import exp
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up',
+             'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 
 def final_result(global_polarity, today_stock, mean):
@@ -209,8 +211,8 @@ def preprocess_tweet(tweet):
     tweet = re.sub('[^a-zA-Z]', ' ', tweet)  # Remove non-alphabetic characters
     tweet = tweet.lower()  # Convert text to lowercase
     tweet = tweet.split()  # Tokenize text
-    tweet = [port_stem.stem(word) for word in tweet if word not in stopwords.words(
-        'english')]  # Remove stopwords and apply stemming
+    tweet = [port_stem.stem(word) for word in tweet if word not in stopwords
+             ]  # Remove stopwords and apply stemming
     tweet = ' '.join(tweet)
     return tweet
 
